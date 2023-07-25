0c56af96cbb2e5007a1d8425af780c9892b7d1ae
cherry0426 <ananya.katpally26@gmail.com>
2023-07-25 15:50:39 -0500

    little leomon all pages

---
+++ src/components/sections/headingPages/About.js
@@ -0,0 +1,16 @@
+export default function About() {
+    return (
+        <article className="about-us">
+            <section className="hero-text">
+                <h1>Little Lemon</h1>
+                <h2>Dallas</h2>
+                <p className="about-subtext">Little Lemon is owned by two Italian brothers, Mario and Adrian, who moved to the United States to pursue their shared dream of owning a restaurant. To craft the menu, Mario relies on family recipes and his experience as a chef in Italy. Adrian does all the marketing for the restaurant and led the effort to expand the menu beyond classic Italian to incorporate additional cuisines from the Mediterranean region..</p>
+            </section>
+
+            <section className="double-image">
+                <img className="about-1" src={require('../../../assets/food/food3.jpeg')} alt="Little Lemon restaurant cuisine 1"></img>
+                <img className="about-2" src={require('../../../assets/food/food2.jpeg')} alt="Little Lemon restaurant cuisine 2"></img>
+            </section>
+    </article>
+    );
+}
